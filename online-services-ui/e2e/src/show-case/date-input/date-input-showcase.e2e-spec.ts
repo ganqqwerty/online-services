07161034a8b24e88cb8f77a0978430d581178372
Lebek Wojciech <Wojciech.Lebek@falconpb.com>
2018-07-02 16:32:42 +0200

    [OS-43] Date Input : added e2e tests, linting corrections

--- online-services-ui/e2e/src/show-case/date-input/date-input-showcase.e2e-spec.ts
+++ online-services-ui/e2e/src/show-case/date-input/date-input-showcase.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { DateInputShowcasePage } from './date-input-showcase.po';
 
 describe('date-input showcase page', () => {
-  const DATE = new Date(1981,6,17).toDateString();
+  const DATE = new Date(1981, 6, 17).toDateString();
 
   let page: DateInputShowcasePage;
 
@@ -13,24 +13,32 @@ describe('date-input showcase page', () => {
   it('should display "From" date if input of "From" field is a valid date', () => {
     page.enterFromDate(DATE);
 
-    expect(page.getFromDateLabel().getText()).toEqual(DATE);
+    expect(page.getFromDateLabel()
+      .getText())
+      .toEqual(DATE);
   });
 
   it('should display "To" date if input of "To" field is a valid date', () => {
     page.enterToDate(DATE);
 
-    expect(page.getToDateLabel().getText()).toEqual(DATE);
+    expect(page.getToDateLabel()
+      .getText())
+      .toEqual(DATE);
   });
 
   it('should display any error if input of "From" field is NOT a valid date', () => {
     page.enterFromDate('i bum cyck cyck');
 
-    expect(page.getFromDateLabel().getText()).toBe('');
+    expect(page.getFromDateLabel()
+      .getText())
+      .toBe('');
   });
 
   it('should display any error if input of "To" field is NOT a valid date', () => {
     page.enterToDate('i tra la la');
 
-    expect(page.getToDateLabel().getText()).toBe('');
+    expect(page.getToDateLabel()
+      .getText())
+      .toBe('');
   });
 });
--- online-services-ui/e2e/src/show-case/date-input/date-input-showcase.po.ts
+++ online-services-ui/e2e/src/show-case/date-input/date-input-showcase.po.ts
@@ -1,8 +1,6 @@
 import { browser, by, element } from 'protractor';
 
 export class DateInputShowcasePage {
-  private fromDatePicker = element(by.id('#fromDatePicker'));
-  private toDatePicker = element(by.id('#toDatePicker'));
 
   navigateTo = () => browser.get('/show-case/date-input');
 
@@ -10,17 +8,8 @@ export class DateInputShowcasePage {
 
   enterToDate = async (text) => element(by.id('toDateInput')).sendKeys(text);
 
-  pickToDate  = async () =>{ this.toDatePicker.click()};
-
-  pickFromDate  = async () =>{ this.fromDatePicker.click()};
-
   getFromDateLabel = () => element(by.id('fromDateLabel'));
 
   getToDateLabel = () => element(by.id('toDateLabel'));
-
-  getFromDateError = () => element(by.id('fromDateError'));
-
-  getToDateError = () => element(by.id('toDateError'));
-
 }
 
