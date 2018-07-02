32eb77c98ff360b5eb01985beb751e2a0998f453
Lebek Wojciech <Wojciech.Lebek@falconpb.com>
2018-07-02 12:00:06 +0200

    [OS-43] Date Input : added tests of toDate and fromDate update

--- online-services-ui/src/app/shared/ui/date-input/date-input.component.spec.ts
+++ online-services-ui/src/app/shared/ui/date-input/date-input.component.spec.ts
@@ -1,7 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DateInputComponent } from './date-input.component';
-
 import { AngularMaterialModule } from '../angular-material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,9 +8,8 @@ import { MatDatepickerInputEvent, MatDatepickerInput } from '@angular/material';
 describe('DateInputComponent', () => {
   let component: DateInputComponent;
   let fixture: ComponentFixture<DateInputComponent>;
-  const DATE = new Date('17-06-1981');
+  const DATE = new Date(1981, 6, 17);
 
-  const createEvent = (date) => new MatDatepickerInputEvent<Date>({} as MatDatepickerInput<Date>, {} as HTMLElement).value = date;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -32,4 +29,31 @@ describe('DateInputComponent', () => {
     expect(component)
       .toBeTruthy();
   });
+
+  it('should updateFromDate emit a date', () => {
+    component.fromDate.subscribe((value) => {
+      console.log(value);
+      console.log(DATE);
+      expect(value)
+        .toBe(DATE);
+    });
+
+    component.updateFromDate(createEvent(DATE));
+  });
+
+  it('should updateToDate emit a date', () => {
+    component.toDate.subscribe((value) => {
+      expect(value)
+        .toBe(DATE);
+    });
+
+    component.updateToDate(createEvent(DATE));
+  });
+
+  function createEvent(date) {
+    const event = new MatDatepickerInputEvent<Date>({} as MatDatepickerInput<Date>, {} as HTMLElement);
+    event.value = date;
+    return event;
+  }
+
 });
