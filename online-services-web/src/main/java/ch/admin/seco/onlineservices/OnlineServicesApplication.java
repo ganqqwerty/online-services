98e7100f2488e430a86637b480c1b1761a08ee60
ivmr <ivan.mrva@gmail.com>
2018-07-03 16:10:54 +0200

    fixed formatting based on the editorconfig rules

--- online-services-web/src/main/java/ch/admin/seco/onlineservices/OnlineServicesApplication.java
+++ online-services-web/src/main/java/ch/admin/seco/onlineservices/OnlineServicesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineServicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineServicesApplication.class, args);
+    }
 }
